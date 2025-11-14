d584c1211644dc57b80c3ec457eb727a3e8c817a
EyzZ <zzyy3534802447@hotmail.com>
2025-11-14 23:22:34 +0800

    完成作业

--- hw0b/src/JavaExercises.java
+++ hw0b/src/JavaExercises.java
@@ -6,7 +6,7 @@ public class JavaExercises {
     /** Returns an array [1, 2, 3, 4, 5, 6] */
     public static int[] makeDice() {
         // TODO: Fill in this function.
-        return null;
+        return new int[]{1,2,3,4,5,6};
     }
 
     /** Returns the order depending on the customer.
@@ -15,15 +15,25 @@ public class JavaExercises {
      *  In any other case, return an empty String[] of size 3. */
     public static String[] takeOrder(String customer) {
         // TODO: Fill in this function.
-        return null;
+        if(customer.equals("Ergun")) return new String[]{"beyti","pizza","hamburger","tea"};
+        else if(customer.equals("Erik")) return new String[]{"sushi","pasta","avocado","coffee"};
+        else return new String[3];
     }
 
     /** Returns the positive difference between the maximum element and minimum element of the given array.
      *  Assumes array is nonempty. */
     public static int findMinMax(int[] array) {
         // TODO: Fill in this function.
-        return 0;
-    }
+        int max=array[0],min=array[0];
+        for(int i=1;i< array.length;i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return max-min;}
 
     /**
       * Uses recursion to compute the hailstone sequence as a list of integers starting from an input number n.
@@ -39,7 +49,17 @@ public class JavaExercises {
 
     private static List<Integer> hailstoneHelper(int x, List<Integer> list) {
         // TODO: Fill in this function.
-        return null;
+        if(x==1){list.add(1);
+        return list;}
+        if(x%2==0){
+            list.add(x);
+            x=x/2;
+            return hailstoneHelper(x,list); }
+        if(x%2!=0){
+            list.add(x);
+            x=x*3+1;
+        return hailstoneHelper(x,list);}
     }
-
 }
+
+
